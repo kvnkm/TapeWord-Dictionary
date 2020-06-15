c2813daa9b10601e703734996533f4af7976ab35
kvnkm <kevinkim391@gmail.com>
2020-06-15 19:16:55 -0400

    Get the text coordinates to log

--- src/view/display.ts
+++ src/view/display.ts
@@ -1,22 +1,22 @@
 import { browser } from "webextension-polyfill-ts";
 import { Definition } from "../background/index";
 
-// window.addEventListener("click", notifyBackground);
-
-// function notifyBackground(e: Event) {
-//   console.log("content-script yo.");
-//   const eventTarget = e.target as HTMLAnchorElement;
-//   if (eventTarget.tagName != "A") {
-//     return;
-//   }
-//   browser.runtime.sendMessage({ url: eventTarget.href });
-// }
-
 function displayDef(def: Definition): void {
   console.log(def);
+  console.log(
+    "------------------------------------------------------------------------"
+  );
 
   const selection: Selection | null = window.getSelection();
   console.log(selection);
+  console.log("Anchor node");
+  console.log(selection?.anchorNode);
+  console.log("Anchor offset");
+  console.log(selection?.anchorOffset);
+  console.log("Range count");
+  console.log(selection?.rangeCount);
+  console.log("Get bounding client rect");
+  console.log(selection?.getRangeAt(0).getBoundingClientRect());
 }
 
 browser.runtime.onMessage.addListener(displayDef);
