a74642759863db316b06c63009c9151fe120fdf8
kvnkm <kevinkim391@gmail.com>
2020-07-07 00:09:12 -0400

    Add nearest-result-searching capability to background script

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,35 +1,89 @@
 import { browser, Menus, Tabs } from "webextension-polyfill-ts";
 
-export interface Definition extends Response {
-  entry: string;
-  request: string;
-  response: string;
-  meaning: {
-    noun: string;
-    verb: string;
-    adverb: string;
-    adjective: string;
-  };
-  ipa: string;
-  version: string;
-  author: string;
-  email: string;
-  result_code: string;
-  result_msg: string;
+interface Definition {
+  def: string;
+  example: string;
 }
 
-async function fetchDef(term: string): Promise<Response> {
-  const endPoint: string = `https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry=${term}`;
+export interface Definitions {
+  [wordType: string]: Definition[];
+}
 
-  const headers = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com",
-      "x-rapidapi-key": "d269648a61msh5775ca37a0c33f1p1bdcdejsnc2172aada844",
-    },
-  };
-  const res: Definition = await (await fetch(endPoint, headers)).json();
-  return res;
+const fetchHeaders = {
+  method: "GET",
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+  },
+};
+
+async function getDefRes(termString: String): Promise<Response> {
+  const endPoint: string = `https://cors-anywhere.herokuapp.com/https://www.lexico.com/en/definition/${termString}`;
+  const defRes: Response = await fetch(endPoint, fetchHeaders);
+  return Promise.resolve(defRes);
+}
+
+async function parseDefRes(defRes: Response): Promise<Definitions> {
+  const parser: DOMParser = new DOMParser();
+  const resDoc: Document = parser.parseFromString(
+    await defRes.text(),
+    "text/html"
+  );
+
+  // Retrieve the definitions by word types
+  let wordTypes: Definitions = {};
+
+  /**
+   * If no exact matches for the term were found, check for nearest result then return that definition
+   * If there are no nearest results, then return empty object
+   */
+  const noMatchFound: Element | undefined = resDoc.querySelectorAll(
+    "div.no-exact-matches"
+  )[0];
+  const nearestResult: Element | undefined = resDoc.querySelectorAll(
+    "a.no-transition"
+  )[0];
+  if (nearestResult) {
+    console.log(nearestResult);
+    const resultString: String = (nearestResult as HTMLAnchorElement).innerText;
+    return Promise.resolve(parseDefRes(await getDefRes(resultString)));
+  }
+  if (noMatchFound) return Promise.resolve(wordTypes);
+
+  const wordTypeSections: NodeListOf<Element> = resDoc.querySelectorAll(
+    "section.gramb"
+  );
+  wordTypeSections.forEach((wordTypeSection) => {
+    const wordType: string = (wordTypeSection.querySelectorAll(
+      "h3.ps.pos > span.pos"
+    )[0] as HTMLElement).innerText;
+    wordTypes[wordType] = [];
+
+    const definitionElements: NodeListOf<Element> = wordTypeSection.querySelectorAll(
+      "ul.semb > li"
+    );
+    definitionElements.forEach((defEl) => {
+      // Continue to next iteration if it's not a real definition
+      const listCheck: NodeListOf<Element> | [] = defEl.querySelectorAll(
+        "p > span.ind"
+      );
+      if (listCheck === undefined || listCheck.length == 0) return;
+
+      // Extract the definition and example strings
+      const def: string = (defEl.querySelectorAll(
+        "p > span.ind"
+      )[0] as HTMLSpanElement)["innerText"];
+      const exampleContainer = defEl.querySelectorAll("div.exg > div.ex")[0];
+      const example: string = exampleContainer
+        ? (exampleContainer as HTMLDivElement).innerText
+        : "";
+      const definition: Definition = { def, example };
+      wordTypes[wordType].push(definition);
+    });
+  });
+
+  return Promise.resolve(wordTypes);
 }
 
 browser.contextMenus.create({
@@ -39,13 +93,21 @@ browser.contextMenus.create({
 });
 
 browser.contextMenus.onClicked.addListener(
-  (info: Menus.OnClickData, tab: Tabs.Tab | undefined): void => {
+  async (
+    info: Menus.OnClickData,
+    tab: Tabs.Tab | undefined
+  ): Promise<undefined> => {
     if (info.menuItemId === "WordStar-search") {
-      console.log(info);
-      const selectionText: string = info.selectionText!.trim();
-      fetchDef(selectionText).then((res) => {
-        browser.tabs.sendMessage(tab?.id || 0, res);
-      });
+      const rawSelectionString: string = info.selectionText!.trim();
+      const selectionText: string = rawSelectionString.split(" ").join("_");
+      const defRes: Response = await getDefRes(selectionText);
+
+      const defs: Definitions = await parseDefRes(defRes);
+      let msg: Definitions | null;
+      Object.keys(defs).length === 0 ? (msg = null) : (msg = defs);
+
+      browser.tabs.sendMessage(tab?.id || 0, msg);
     }
+    return;
   }
 );
--- src/view/display.ts
+++ src/view/display.ts
@@ -1,22 +1,94 @@
 import { browser } from "webextension-polyfill-ts";
-import { Definition } from "../background/index";
+import { Definitions } from "../background/index";
 
-function displayDef(def: Definition): void {
-  console.log(def);
-  console.log(
-    "------------------------------------------------------------------------"
-  );
+/**
+ * TODOS
+ * ✔️ Receive (definitions: WordTypes)
+ * (2) Place each type+definition into its own containers
+ * (3) Determine definition-box dimensions
+ *     - Max-content the width and height if possible to do so without hitting window edges
+ *       - Else- expand towards the edge(s) and leave buffer(s) of 50px
+ *             - enable overflows
+ * (4) Determine placement
+ *     - Utilize window.innerWidth & window.innerHeight
+ *     - Create four placement-helper-functions for dynamic placements around DOMRects (eg. placeBelowRight, placeBelowLeft, placeAboveRight, placeAboveLeft)
+ *       - Always call placeBelowRight unless:
+ *         - There's no space for the element below - then call placeAboveRight
+ *         - There's no space for the element both below and to the right - then call placeAboveLeft
+ */
 
-  const selection: Selection | null = window.getSelection();
-  console.log(selection);
-  console.log("Anchor node");
-  console.log(selection?.anchorNode);
-  console.log("Anchor offset");
-  console.log(selection?.anchorOffset);
-  console.log("Range count");
-  console.log(selection?.rangeCount);
-  console.log("Get bounding client rect");
-  console.log(selection?.getRangeAt(0).getBoundingClientRect());
+function generateNullFrame(): HTMLDivElement {
+  const frame: HTMLDivElement = document.createElement("div");
+  frame.className = "null-frame";
+  frame.innerText = "No definition found :(";
+  frame.style.display = "flex";
+  frame.style.justifyContent = "center";
+  frame.style.background = "rgba(247, 118, 157, 0.8);";
+  /** bookmarked */
+
+  return frame;
 }
 
-browser.runtime.onMessage.addListener(displayDef);
+function generateDefFrame(defs: Definitions): HTMLDivElement {
+  /**
+   * TODOS (2)
+   */
+  // Create main container
+  const frame: HTMLDivElement = document.createElement("div");
+  frame.className = "definitions-frame";
+
+  // Build types-control component
+  const typesControl: HTMLDivElement = document.createElement("div");
+  const upArrow: HTMLDivElement = document.createElement("div");
+  const wordType: HTMLHeadingElement = document.createElement("h1");
+  const downArrow: HTMLDivElement = document.createElement("div");
+  typesControl.className = "types";
+  upArrow.className = "types__up-arrow";
+  wordType.className = "types__label";
+  downArrow.className = "types__down-arrow";
+
+  // Build definitions component
+  const definitions: HTMLDivElement = document.createElement("div");
+  const definition: HTMLParagraphElement = document.createElement("p");
+  const example: HTMLParagraphElement = document.createElement("p");
+  const definitionsControl: HTMLDivElement = document.createElement("div");
+  const leftArrow: HTMLDivElement = document.createElement("div");
+  const scrollLabel: HTMLParagraphElement = document.createElement("p");
+  const rightArrow: HTMLDivElement = document.createElement("div");
+  definitions.className = "definitions";
+  definition.className = "definitions__definition";
+  example.className = "definitions__example";
+  definitionsControl.className = "definitions__control";
+  leftArrow.className = "definitions__left-arrow";
+  scrollLabel.className = "definitions__scroll-label";
+  rightArrow.className = "definitions__right-arrow";
+
+  // Compose frame
+  typesControl.appendChild(upArrow);
+  typesControl.appendChild(wordType);
+  typesControl.appendChild(downArrow);
+  definitions.appendChild(definition);
+  definitions.appendChild(example);
+  definitionsControl.appendChild(leftArrow);
+  definitionsControl.appendChild(scrollLabel);
+  definitionsControl.appendChild(rightArrow);
+  definitions.appendChild(definitionsControl);
+  frame.appendChild(typesControl);
+  frame.appendChild(definitions);
+
+  return frame;
+}
+
+browser.runtime.onMessage.addListener((defs) => {
+  /**
+   * Generate complete frame if definition exists
+   * If not- generate default message frame
+   */
+  console.log(defs);
+  let frame: HTMLDivElement;
+  if (defs) {
+    frame = generateDefFrame(defs);
+  } else {
+    frame = generateNullFrame();
+  }
+});
